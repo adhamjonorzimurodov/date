e51d7f1d34437e7687e45a789c659d346510463e
adhamjonorzimurodov <adhamjonorzimurodov7@gmail.com>
2022-12-27 18:23:03 +0900

    date project

---
+++ index.js
@@ -0,0 +1,19 @@
+const starDate = document.getElementById("start-date");
+const endDate = document.getElementById("end-date");
+const button = document.getElementById("button");
+const output = document.getElementById("output");
+
+const startErrMsg = document.getElementById("errormsg-start");
+const endErrMsg = document.getElementById("errormsg-end");
+
+
+
+button.addEventListener("click", getDays);
+
+
+function getDays(e) {
+  if (e)
+  console starDate = new Date(start);
+  const endDate = new Date(end);
+   const re
+}
